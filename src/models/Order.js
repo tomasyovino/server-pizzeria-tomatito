2d69033362504796cffdcf147cd1aaa399cc95c3
tomasyovino <tomasyovino@gmail.com>
2023-03-25 13:30:19 -0300

    Add Product & Order models Schemas

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const orderSchema = mongoose.Schema({
-
-});
+    products: { type: Array, required: true, max: 100 },
+    amount: { type: Number, required: true },
+    format: { type: String, required: true },
+    payWith: { type: String, required: true }
+}, { timestamps: true });
 
 export const OrderModel = mongoose.model("Order", orderSchema);
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema({
-
-});
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true},
+    description: { type: String, required: true }
+}, { timestamps: true });
 
 export const ProductModel = mongoose.model("Product", productSchema);
